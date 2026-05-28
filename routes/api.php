88a68f6766a5c17dd936df8b5024ebfd4085e94d
debapriyacodeclouds <debapriya.bose@codeclouds.in>
2026-05-29 00:38:29 +0530

    api route issue fixed

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,12 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ExpenseCheckController;
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
 
 Route::get('/expense-check', [ExpenseCheckController::class, 'check']);
