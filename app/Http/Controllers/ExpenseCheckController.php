64edf66e5fdfddf141b8ccb1a0b1faa1c5e900f1
debapriyacodeclouds <debapriya.bose@codeclouds.in>
2026-05-29 01:01:07 +0530

    carbon added for today

--- app/Http/Controllers/ExpenseCheckController.php
+++ app/Http/Controllers/ExpenseCheckController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Expense;
+use Carbon\Carbon;
 
 class ExpenseCheckController extends Controller
 {
     
     public function check()
     {
+        $today = Carbon::now('Asia/Kolkata')->toDateString();
         $exists = Expense::whereDate('created_at', today())->exists();
 
         return response()->json([
